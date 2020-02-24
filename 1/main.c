3dab412ec3a8d036da4a3778777e987bb504ccec
ZukoAP <akmurzinpavel@gmail.com>
2020-02-24 15:51:39 +0300

    f

--- 1/main.c
+++ 1/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    puts("hello");
-    return 0;
+struct vector
+{
+    int *coordinates;
+};
+void createVector(struct vector* newVector, int dimensionSize){
+    newVector->coordinates = (int*) malloc(dimensionSize*sizeof(int));
+    puts("Enter coordinates of the new vector");
+    for(int currCoord=0; currCoord<dimensionSize; currCoord++){
+        printf("e%d = ",currCoord+1);
     }
+
+}
+
+int main(){
+    printf("Vector size: %llu \n", sizeof(struct date));
+    puts("Put dimension size")
+//    system ( "Pause" );
+    return 0;
+}
